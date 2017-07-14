a453889145d6586396f6a1c037052d04bca2fb39
runmanz <296413715@qq.com>
2017-07-14 17:57:04 +0800

    USER TEST

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -12,25 +12,57 @@ use Yii;
 use app\models\User;
 use yii\rest\Controller;
 use api\models\SignupForm;
+use yii\filters\auth\HttpBasicAuth;
 
 class UserController extends Controller
 {
+    public $classClass;
+    public $authority = 'isAuthor';
+   /* public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => HttpBasicAuth::className(),
+        ];
+        return $behaviors;
+    }*/
+    public function execute($user, $item, $params)
+    {
+        return isset($params['post']) ? $params['post']->createdBy == $user : false;
+    }
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        // 禁用"delete" 和 "create" 动作
+        unset($actions['delete'], $actions['create']);
+
+        // 使用"prepareDataProvider()"方法自定义数据provider
+        //$actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+
+        return $actions;
+    }
 
     public function actionIndex(){
-        return User::find()->limit(10)->all();
+        if(yii::$app->user->can('updatePost')){
+            return User::find()->limit(10)->all();
+        }
+        return ['msg'=>'U can\'t see them'];
     }
 
     public function actionCreate(){
         $model = new SignupForm();
-
-        if ($model->load(Yii::$app->request->post())) {
-            //var_dump($model);die;
-            if ($user = $model->signup()) {
-                if (Yii::$app->getUser()->login($user)) {
-                    return ['msg' => 'successful'];
+        if(yii::$app->user->can('createPost')){
+            if ($model->load(Yii::$app->request->post())) {
+                //var_dump($model);die;
+                if ($user = $model->signup()) {
+                    if (Yii::$app->getUser()->login($user)) {
+                        return ['msg' => 'successful'];
+                    }
                 }
             }
+            return ['msg' => $model->getErrors()];
         }
-        return ['msg' => $model->getErrors()];
+        return ['msg' => 'U can\'t  register in my website'];
     }
 }
